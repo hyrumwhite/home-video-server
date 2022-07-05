d7eb0c9635a67e857f561afc6b03c55122e0136e
Seth White <seth.white@strakertranslations.com>
2022-07-05 10:57:56 -0400

    minor updates

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 import express from "express";
 import { videolist } from "./videolist.js";
 import { videostream } from "./videostream.js";
-import { uploadVideo } from "./uploadVideo.js";
+// import { uploadVideo } from "./uploadVideo.js";
 import { resolve } from "path";
 import compression from "compression";
 import { v4 as uuidv4 } from "uuid";
 import multer from "multer";
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    if (file.mimetype.includes("video")) {
-      cb(null, "./videos");
-    } else {
-      cb(null, "./images");
-    }
-  },
-  filename: function (req, file, cb) {
-    cb(null, `temp-${uuidv4()}.${file.originalname.split(".")[1]}`);
-  },
+	destination: function (req, file, cb) {
+		if (file.mimetype.includes("video")) {
+			cb(null, "./videos");
+		} else {
+			cb(null, "./images");
+		}
+	},
+	filename: function (req, file, cb) {
+		cb(null, `temp-${uuidv4()}.${file.originalname.split(".")[1]}`);
+	},
 });
 const upload = multer({ storage });
 const videoUpload = upload.fields([{ name: "video" }, { name: "thumbnail" }]);
@@ -27,14 +27,14 @@ app.use(compression());
 
 const PORT = 8089;
 
-app.post("/video", videoUpload, uploadVideo);
+// app.post("/video", videoUpload, uploadVideo);
 app.get("/video", videolist);
 app.get("/video/:filename", videostream);
 app.get("/upload", (req, res) => {
-  res.sendFile(`${resolve()}/public/upload.html`);
+	res.sendFile(`${resolve()}/public/upload.html`);
 });
 app.get("/test", (req, res) => {
-  res.sendFile(`${resolve()}/public/test.html`);
+	res.sendFile(`${resolve()}/public/test.html`);
 });
 
 app.get("/components/*", express.static("./public"));
@@ -43,7 +43,7 @@ app.get("/css/*", express.static("./public"));
 app.get("/html/*", express.static("./public"));
 app.get("/thumbnails/*", express.static("./public"));
 app.get("*", (req, res) => {
-  res.sendFile(`${resolve()}/public/index.html`);
+	res.sendFile(`${resolve()}/public/index.html`);
 });
 
 app.listen(PORT);
--- public/components/GenreSliders.js
+++ public/components/GenreSliders.js
@@ -5,88 +5,89 @@ import { ErgoElement } from "../assets/ErgoElement.min.js";
 let slots = document.querySelectorAll("[slot='GenreSliders']");
 
 const getData = () =>
-  new Promise((resolve, reject) => {
-    window.setTimeout(() => {
-      resolve([
-        {
-          genre: "Action",
-          movies: [
-            { name: "Wolfwalkers", id: "wolfwalkers", thumbnail: true },
-            { name: "Song of the Sea", id: "songofthesea" },
-            { name: "Secret of Kells", id: "secretofkells" },
-            { name: "Indiana Jones", id: 12347 },
-            { name: "Twilight", id: 1238 },
-            { name: "Free Solo", id: 1239 },
-            { name: "Shrek", id: 1240 },
-          ],
-        },
-        {
-          genre: "Fantasy",
-          movies: [
-            { name: "Wolfwalkers", id: 1234 },
-            { name: "Predator", id: 1235 },
-            { name: "Psycho Goreman", id: 1236 },
-            { name: "Indiana Jones", id: 12347 },
-            { name: "Twilight", id: 1238 },
-            { name: "Free Solo", id: 1239 },
-            { name: "Shrek", id: 1240 },
-          ],
-        },
-      ]);
-    }, 200);
-  });
+	new Promise((resolve, reject) => {
+		window.setTimeout(() => {
+			resolve([
+				{
+					genre: "Action",
+					movies: [
+						{ name: "Wolfwalkers", id: "wolfwalkers", thumbnail: true },
+						{ name: "Song of the Sea", id: "songofthesea" },
+						{ name: "Secret of Kells", id: "secretofkells" },
+						{ name: "The Bad Guys", id: "badguys", thumbnail: true },
+						{ name: "Barbie: Pink Shoes", id: "barbieinthepinkshoes" },
+						{ name: "Twilight", id: 1238 },
+						{ name: "Free Solo", id: 1239 },
+						{ name: "Shrek", id: 1240 },
+					],
+				},
+				{
+					genre: "Fantasy",
+					movies: [
+						{ name: "Wolfwalkers", id: 1234 },
+						{ name: "Predator", id: 1235 },
+						{ name: "Psycho Goreman", id: 1236 },
+						{ name: "Indiana Jones", id: 12347 },
+						{ name: "Twilight", id: 1238 },
+						{ name: "Free Solo", id: 1239 },
+						{ name: "Shrek", id: 1240 },
+					],
+				},
+			]);
+		}, 200);
+	});
 
 const handleAnchorClick = ($event) => {
-  if (!$event.ctrlKey) {
-    events.dispatch(
-      "movie-click",
-      $event.target.closest("a").getAttribute("key")
-    );
-    $event.preventDefault();
-    $event.stopPropagation();
-  }
+	if (!$event.ctrlKey) {
+		events.dispatch(
+			"movie-click",
+			$event.target.closest("a").getAttribute("key")
+		);
+		$event.preventDefault();
+		$event.stopPropagation();
+	}
 };
 
 const generateSliders = async (slot) => {
-  let genres = await getData();
-  let template = document.querySelector("#GenreSliderTemplate");
-  for (let genre of genres) {
-    let {
-      self: nav,
-      h1,
-      ".horizontal-scroller": horizontalScroller,
-      "a:first-child": firstAnchor,
-      "$a.placeholder": placeholders,
-    } = ErgoElement(template.cloneNode(true));
+	let genres = await getData();
+	let template = document.querySelector("#GenreSliderTemplate");
+	for (let genre of genres) {
+		let {
+			self: nav,
+			h1,
+			".horizontal-scroller": horizontalScroller,
+			"a:first-child": firstAnchor,
+			"$a.placeholder": placeholders,
+		} = ErgoElement(template.cloneNode(true));
 
-    h1.textContent = genre.genre;
-    h1.classList.remove("placeholder");
+		h1.textContent = genre.genre;
+		h1.classList.remove("placeholder");
 
-    for (let movie of genre.movies) {
-      let anchor = firstAnchor.cloneNode(Boolean(movie.thumbnail));
-      anchor.href = `/watch/${movie.id}`;
-      anchor.setAttribute("key", movie.id);
-      anchor.classList.remove("placeholder");
-      if (movie.thumbnail) {
-        let img = anchor.querySelector("img");
-        img.src = `/thumbnails/${movie.id}.jpg`;
-        anchor.appendChild(img);
-      } else {
-        anchor.textContent = movie.name;
-      }
-      anchor.addEventListener("click", handleAnchorClick);
-      horizontalScroller.appendChild(anchor);
-    }
-    placeholders.remove();
-    slot.parentElement.insertBefore(nav, slot);
-  }
-  template.remove();
-  slot.remove();
-  events.dispatch("genres-loaded");
+		for (let movie of genre.movies) {
+			let anchor = firstAnchor.cloneNode(Boolean(movie.thumbnail));
+			anchor.href = `/watch/${movie.id}`;
+			anchor.setAttribute("key", movie.id);
+			anchor.classList.remove("placeholder");
+			if (movie.thumbnail) {
+				let img = anchor.querySelector("img");
+				img.src = `/thumbnails/${movie.id}.jpg`;
+				anchor.appendChild(img);
+			} else {
+				anchor.textContent = movie.name;
+			}
+			anchor.addEventListener("click", handleAnchorClick);
+			horizontalScroller.appendChild(anchor);
+		}
+		placeholders.remove();
+		slot.parentElement.insertBefore(nav, slot);
+	}
+	template.remove();
+	slot.remove();
+	events.dispatch("genres-loaded");
 };
 
 if (slots.length) {
-  for (let slot of slots) {
-    generateSliders(slot);
-  }
+	for (let slot of slots) {
+		generateSliders(slot);
+	}
 }
